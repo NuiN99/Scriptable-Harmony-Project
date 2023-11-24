b5d73c8215cc364bb95bb2ac96dec1efe2fc7e73
NuiN99 <endyverse@gmail.com>
2023-11-23 21:11:47 -0800

    Alignment and removed filler

--- Scriptable Variables/Assets/Plugins/NuiN/ScriptableVariables/Editor/ReferenceVariableDrawerBase.cs
+++ Scriptable Variables/Assets/Plugins/NuiN/ScriptableVariables/Editor/ReferenceVariableDrawerBase.cs
@@ -31,7 +31,10 @@ namespace NuiN.ScriptableVariables.Editor
                 Rect helpBoxPosition = new Rect(position.x + position.width / 2f, position.y, position.width / 2f, EditorGUIUtility.singleLineHeight);
                 EditorGUI.HelpBox(helpBoxPosition, $"Missing {typeName}SO", MessageType.Warning);
             }
-
+            
+            // align the label to account for the extra space the dropdown arrow takes up
+            position.x -= 13;
+            
             bool isExpanded = EditorGUI.PropertyField(position, property, label, true);
             if (isExpanded)
             {
@@ -54,19 +57,14 @@ namespace NuiN.ScriptableVariables.Editor
     public class ReadVariableDrawer : ReferenceVariableDrawerBase
     {
         protected override SerializedProperty GetVariableProperty(SerializedProperty property)
-        {
-            ReadVariable<object> classInstance = new();
-            return property.FindPropertyRelative("readReference");
-        }
+            => property.FindPropertyRelative("readReference");
     }
 
     [CustomPropertyDrawer(typeof(WriteVariable<>))]
     public class WriteVariableDrawer : ReferenceVariableDrawerBase
     {
         protected override SerializedProperty GetVariableProperty(SerializedProperty property)
-        {
-            return property.FindPropertyRelative("writeReference");
-        }
+            => property.FindPropertyRelative("writeReference");
     }  
 }
 
