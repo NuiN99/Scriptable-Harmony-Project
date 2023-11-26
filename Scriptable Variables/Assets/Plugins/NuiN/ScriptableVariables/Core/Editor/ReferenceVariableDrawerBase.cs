3d1ee8319f8ba73abfcb1e26f54ae570410025c3
NuiN99 <endyverse@gmail.com>
2023-11-26 00:05:30 -0800

    Refactored

--- Scriptable Variables/Assets/Plugins/NuiN/ScriptableVariables/Core/Editor/ReferenceVariableDrawerBase.cs
+++ Scriptable Variables/Assets/Plugins/NuiN/ScriptableVariables/Core/Editor/ReferenceVariableDrawerBase.cs
@@ -7,14 +7,13 @@ namespace NuiN.ScriptableVariables.Editor
 {
     public abstract class ReferenceVariableDrawerBase : PropertyDrawer
     {
+        Color _readColor = new Color(0.7f, 0.9f, 0.95f, 1f);
+        Color _writeColor = new Color(0.9f, 0.7f, 0.7f, 1f);
+        
         protected abstract SerializedProperty GetVariableProperty(SerializedProperty property);
 
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-        {
-            SerializedProperty variableProperty = GetVariableProperty(property);
-            if (variableProperty.objectReferenceValue == null) return EditorGUIUtility.singleLineHeight;
-            return EditorGUIUtility.singleLineHeight;
-        }
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) 
+            => EditorGUIUtility.singleLineHeight;
 
         static string GetReadableTypeName(Type type)
         {
@@ -34,13 +33,6 @@ namespace NuiN.ScriptableVariables.Editor
 
             SerializedProperty variableProperty = GetVariableProperty(property);
 
-            Color labelTextColor = variableProperty.name switch
-            {
-                "readReference" => new Color(0.7f, 0.9f, 0.95f, 1f),
-                "writeReference" => new Color(0.9f, 0.7f, 0.7f, 1f),
-                _ => Color.white
-            };
-
             Type variableType = fieldInfo.FieldType.GetGenericArguments()[0];
             string typeName = GetReadableTypeName(variableType);
 
@@ -58,52 +50,27 @@ namespace NuiN.ScriptableVariables.Editor
 
             if (variableProperty.objectReferenceValue == null)
             {
-                Color ogColor = EditorStyles.label.normal.textColor;
-                EditorStyles.label.normal.textColor = labelTextColor;
+                DrawLabel();
 
-                Rect labelPosition = new Rect(position.x, position.y, position.width - EditorGUIUtility.singleLineHeight, EditorGUIUtility.singleLineHeight);
-                EditorGUI.LabelField(labelPosition, label);
+                DrawFindButton();
 
-                EditorStyles.label.normal.textColor = ogColor;
-
-                Rect buttonPosition = new Rect(position.x + position.width - EditorGUIUtility.singleLineHeight, position.y, EditorGUIUtility.singleLineHeight, EditorGUIUtility.singleLineHeight);
-                GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
-                GUIContent buttonText = new GUIContent("^");
-
-                Color originalColor = GUI.backgroundColor;
-                GUI.backgroundColor = new Color(0.4f, 0.4f, 0.8f, 1.0f);
-                if (GUI.Button(buttonPosition, buttonText, buttonStyle))
-                {
-                    ScriptableVariableFinder.OpenFindWindow(typeName, variableProperty);
-                }
-                GUI.backgroundColor = originalColor;
-
-                position.y += EditorGUIUtility.singleLineHeight;
-                float labelWidth = EditorGUIUtility.labelWidth;
-                float helpBoxWidth = position.width - labelWidth - EditorGUIUtility.singleLineHeight; // Adjust width to make room for the button
-
-                Rect helpBoxPosition = new Rect(position.x + labelWidth, position.y - EditorGUIUtility.singleLineHeight, helpBoxWidth, EditorGUIUtility.singleLineHeight);
-                EditorGUI.HelpBox(helpBoxPosition, $"Unassigned - {typeName}", MessageType.Warning);
+                DrawHelpBox();
             }
             else
             {
-                Rect objectFieldPosition = new Rect(position.x, position.y, position.width - EditorGUIUtility.singleLineHeight, EditorGUIUtility.singleLineHeight);
+                DrawPropertyField();
 
-                Color ogColor = EditorStyles.label.normal.textColor;
-                EditorStyles.label.normal.textColor = labelTextColor;
+                DrawFindButton();
+            }
 
-                EditorGUI.BeginProperty(objectFieldPosition, GUIContent.none, variableProperty);
-                EditorGUI.PropertyField(objectFieldPosition, variableProperty, label, true);
-                EditorGUI.EndProperty();
+            EditorGUI.EndProperty();
+            return;
 
-                EditorStyles.label.normal.textColor = ogColor;
-
+            void DrawFindButton()
+            {
                 Rect buttonPosition = new Rect(position.x + position.width - EditorGUIUtility.singleLineHeight, position.y, EditorGUIUtility.singleLineHeight, EditorGUIUtility.singleLineHeight);
-
                 GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
-
                 GUIContent buttonText = new GUIContent("^");
-
                 Color originalColor = GUI.backgroundColor;
                 GUI.backgroundColor = new Color(0.4f, 0.4f, 0.8f, 1.0f);
                 if (GUI.Button(buttonPosition, buttonText, buttonStyle))
@@ -113,7 +80,42 @@ namespace NuiN.ScriptableVariables.Editor
                 GUI.backgroundColor = originalColor;
             }
 
-            EditorGUI.EndProperty();
+            void DrawHelpBox()
+            {
+                position.y += EditorGUIUtility.singleLineHeight;
+                float labelWidth = EditorGUIUtility.labelWidth;
+                float helpBoxWidth = position.width - labelWidth - EditorGUIUtility.singleLineHeight; // Adjust width to make room for the button
+                Rect helpBoxPosition = new Rect(position.x + labelWidth, position.y - EditorGUIUtility.singleLineHeight, helpBoxWidth, EditorGUIUtility.singleLineHeight);
+                EditorGUI.HelpBox(helpBoxPosition, $"Unassigned - {typeName}", MessageType.Warning);
+            }
+
+            void DrawLabel()
+            {
+                EditorStyles.label.normal.textColor = LabelTextColor();
+                Rect labelPosition = new Rect(position.x, position.y, position.width - EditorGUIUtility.singleLineHeight, EditorGUIUtility.singleLineHeight);
+                EditorGUI.LabelField(labelPosition, label);
+                EditorStyles.label.normal.textColor = Color.white;
+            }
+
+            void DrawPropertyField()
+            {
+                Rect objectFieldPosition = new Rect(position.x, position.y, position.width - EditorGUIUtility.singleLineHeight, EditorGUIUtility.singleLineHeight);
+                EditorStyles.label.normal.textColor = LabelTextColor();
+                EditorGUI.BeginProperty(objectFieldPosition, GUIContent.none, variableProperty);
+                EditorGUI.PropertyField(objectFieldPosition, variableProperty, label, true);
+                EditorGUI.EndProperty();
+                EditorStyles.label.normal.textColor = Color.white;
+            }
+
+            Color LabelTextColor()
+            {
+                return variableProperty.name switch
+                {
+                    "readReference" => _readColor,
+                    "writeReference" => _writeColor,
+                    _ => Color.white
+                };
+            }
         }
     }
 
