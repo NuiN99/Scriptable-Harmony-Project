a7461d0d4bb42e761b21b991f6558520e5d683ed
NuiN99 <endyverse@gmail.com>
2023-12-09 02:41:20 -0800

    Serializable Dictionary for later

---
+++ Scriptable Variables/Assets/NuiN/ScriptableVariables/Core/Internal/Helpers/SerializableDictionary.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NuiN.ScriptableVariables.Internal.Helpers
+{
+    [Serializable]
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField] List<TKey> keys;
+        [SerializeField] List<TValue> values;
+
+        public void OnBeforeSerialize()
+        {
+            keys.Clear();
+            values.Clear();
+            foreach (KeyValuePair<TKey, TValue> pair in this)
+            {
+                keys.Add(pair.Key);
+                values.Add(pair.Value);
+            }
+        }
+
+        public void OnAfterDeserialize()
+        {
+            Clear();
+
+            if (keys.Count != values.Count) throw new Exception(string.Format($"there are {keys.Count} keys and {values.Count} values after deserialization. Make sure that both key and value types are serializable."));
+
+            for (int i = 0; i < keys.Count; i++) this.Add(keys[i], values[i]);
+        }
+    }
+}
