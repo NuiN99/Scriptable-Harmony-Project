913f149a7eb9adb6c1cd58dcd85c26393b96331f
NuiN99 <endyverse@gmail.com>
2023-12-10 00:49:09 -0800

    PlayAll audio clips in list

--- Scriptable Harmony Project/Assets/Plugins/NuiN/ScriptableHarmony/Sound/SoundData/SoundArraySO.cs
+++ Scriptable Harmony Project/Assets/Plugins/NuiN/ScriptableHarmony/Sound/SoundData/SoundArraySO.cs
@@ -16,6 +16,11 @@ namespace NuiN.ScriptableHarmony.Sound
         public void PlayIndex(int index, float volumeFactor = 1)
             => player.Play(audioClips[index], volume * volumeFactor);
 
+        public void PlayAll(float volumeFactor = 1)
+        {
+            foreach(var clip in audioClips) player.Play(clip, volume * volumeFactor);
+        }
+
         public void PlayIndexSpatial(int index, Vector3 position, Transform parent = null, float volumeFactor = 1)
             => player.PlaySpatial(audioClips[index], position, volume * volumeFactor, parent);
         public void PlayIndexSpatial(int index, GetVariable<Vector3> position, Transform parent = null, float volumeFactor = 1)
@@ -26,5 +31,26 @@ namespace NuiN.ScriptableHarmony.Sound
             => player.PlaySpatial(audioClips[index], position, volume * volumeFactor, parent);
         public void PlayIndexSpatial(int index, SetVariable<Vector2> position, Transform parent = null, float volumeFactor = 1)
             => player.PlaySpatial(audioClips[index], position, volume * volumeFactor, parent);
+        
+        public void PlayAllSpatial(Vector3 position, Transform parent = null, float volumeFactor = 1)
+        {
+            foreach(var clip in audioClips) player.PlaySpatial(clip, position, volume * volumeFactor, parent);
+        }
+        public void PlayAllSpatial(GetVariable<Vector3> position, Transform parent = null, float volumeFactor = 1)
+        {
+            foreach(var clip in audioClips) player.PlaySpatial(clip, position, volume * volumeFactor, parent);
+        }
+        public void PlayAllSpatial(SetVariable<Vector3> position, Transform parent = null, float volumeFactor = 1)
+        {
+            foreach(var clip in audioClips) player.PlaySpatial(clip, position, volume * volumeFactor, parent);
+        }
+        public void PlayAllSpatial(GetVariable<Vector2> position, Transform parent = null, float volumeFactor = 1)
+        {
+            foreach(var clip in audioClips) player.PlaySpatial(clip, position, volume * volumeFactor, parent);
+        }
+        public void PlayAllSpatial(SetVariable<Vector2> position, Transform parent = null, float volumeFactor = 1)
+        {
+            foreach(var clip in audioClips) player.PlaySpatial(clip, position, volume * volumeFactor, parent);
+        }
     }
 }
