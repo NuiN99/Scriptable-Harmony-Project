721164dceab9f642ca4c8589234db439f3c9b993
NuiN99 <endyverse@gmail.com>
2023-12-10 01:06:52 -0800

    Testing

--- Scriptable Harmony Project/Assets/Plugins/NuiN/ScriptableHarmony/Sound/SoundData/SoundSO.cs
+++ Scriptable Harmony Project/Assets/Plugins/NuiN/ScriptableHarmony/Sound/SoundData/SoundSO.cs
@@ -10,5 +10,15 @@ namespace NuiN.ScriptableHarmony.Sound
         public AudioClip Clip => audioClip;
         
         protected override AudioClip GetClip() => audioClip;
+
+        // assigns it correctly before naming, but after naming the assignment dissapears?
+        /*[Conditional("UNITY_EDITOR")]
+        void Awake()
+        {
+            // sets the field to the current selection in the editor
+            if (audioClip != null || Selection.activeObject == null || Selection.activeObject is not AudioClip) return;
+            audioClip = Selection.activeObject as AudioClip;
+            EditorUtility.SetDirty(this);
+        }*/
     }
 }
