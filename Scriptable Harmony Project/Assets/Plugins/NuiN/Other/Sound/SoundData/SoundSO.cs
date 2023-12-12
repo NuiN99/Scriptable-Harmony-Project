fb31b3e4deb245c8c2e12ce8c84029ade1d042c4
NuiN99 <endyverse@gmail.com>
2023-12-11 22:10:14 -0800

    Moved files

---
+++ Scriptable Harmony Project/Assets/Plugins/NuiN/Other/Sound/SoundData/SoundSO.cs
@@ -0,0 +1,15 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace NuiN.ScriptableHarmony.Sound
+{
+    [CreateAssetMenu(menuName = "ScriptableHarmony/Sound/Sound", fileName = "New Sound")]
+    public class SoundSO : SoundBaseSO
+    {
+        [SerializeField] AudioClip audioClip;
+
+        public AudioClip Clip => audioClip;
+        
+        protected override AudioClip GetClip() => audioClip;
+    }
+}
