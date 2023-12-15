6084e0d5ea4f4ea59c76554767c745f32ca7bbf6
NuiN99 <endyverse@gmail.com>
2023-12-14 17:08:46 -0800

    Update RuntimeObjectBaseSO.cs

--- Scriptable Harmony Project/Assets/Plugins/NuiN/ScriptableHarmony/Variables/Internal/BaseClasses/RuntimeObjectBaseSO.cs
+++ Scriptable Harmony Project/Assets/Plugins/NuiN/ScriptableHarmony/Variables/Internal/BaseClasses/RuntimeObjectBaseSO.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 namespace NuiN.ScriptableHarmony.Base
 {
-    public abstract class RuntimeObjectBaseSO<T> : ScriptableObjectBaseSO<T> where T : class
+    public abstract class RuntimeObjectBaseSO<T> : ScriptableObjectBaseSO<T> where T : Object
     {
         protected abstract RuntimeObjectReferencesContainer ComponentHolders { get; set; }
     
