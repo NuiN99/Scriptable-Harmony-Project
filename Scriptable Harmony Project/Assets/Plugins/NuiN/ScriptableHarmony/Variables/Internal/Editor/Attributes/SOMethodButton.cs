176d335a4d31dc9960a9a9a4940bc437edad5171
NuiN99 <endyverse@gmail.com>
2023-12-23 15:09:39 -0800

    Update SOMethodButton.cs

--- Scriptable Harmony Project/Assets/Plugins/NuiN/ScriptableHarmony/Variables/Internal/Editor/Attributes/SOMethodButton.cs
+++ Scriptable Harmony Project/Assets/Plugins/NuiN/ScriptableHarmony/Variables/Internal/Editor/Attributes/SOMethodButton.cs
@@ -3,16 +3,17 @@ using System.Linq;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace NuiN.ScriptableHarmony.Editor.Attributes
 {
     [AttributeUsage(AttributeTargets.Method)]
-    public class SOMethodButton : PropertyAttribute
+    public class MethodButtonAttribute : PropertyAttribute
     {
         public readonly string label;
         public readonly bool onlyShowInPlayMode;
         public readonly object[] parameters;
-        public SOMethodButton(string label, bool onlyShowInPlayMode = false, object[] parameters = null)
+        public MethodButtonAttribute(string label, bool onlyShowInPlayMode = false, object[] parameters = null)
         {
             this.label = label;
             this.parameters = parameters;
@@ -21,22 +22,22 @@ namespace NuiN.ScriptableHarmony.Editor.Attributes
     }
     
 #if UNITY_EDITOR
-    [CustomEditor(typeof(ScriptableObject), true)]
-    public class EditorButtonDrawer : UnityEditor.Editor
+    [CustomEditor(typeof(Object), true)]
+    public class MethodButtonAttributeDrawer : UnityEditor.Editor
     {
         public override void OnInspectorGUI()
         {
-            ScriptableObject script = (ScriptableObject)target;
+            Object script = (Object)target;
             MethodInfo[] methods = script.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
-                .Where(method => method.GetCustomAttributes(typeof(SOMethodButton), true).Length > 0)
+                .Where(method => method.GetCustomAttributes(typeof(MethodButtonAttribute), true).Length > 0)
                 .ToArray();
 
             foreach (var method in methods)
             {
-                SOMethodButton attribute = (SOMethodButton)method.GetCustomAttributes(typeof(SOMethodButton), true)[0];
+                MethodButtonAttribute attribute = (MethodButtonAttribute)method.GetCustomAttributes(typeof(MethodButtonAttribute), true)[0];
                 string buttonLabel = attribute == null ? method.Name : attribute.label;
 
-                if (attribute == null || (!Application.isPlaying && !attribute.onlyShowInPlayMode)) continue;
+                if (attribute == null || (attribute.onlyShowInPlayMode && !Application.isPlaying)) continue;
                 if (GUILayout.Button(buttonLabel)) method.Invoke(script, attribute?.parameters);
             }
             
