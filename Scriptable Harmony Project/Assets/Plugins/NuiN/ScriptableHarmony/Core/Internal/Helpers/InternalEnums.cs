7b06722627c27d4855271fb478c09d73ac3d6879
NuiN99 <endyverse@gmail.com>
2023-12-24 01:52:42 -0800

    Moved to package structure

--- Scriptable Harmony Project/Assets/Plugins/NuiN/ScriptableHarmony/Core/Internal/Helpers/InternalEnums.cs
+++
@@ -1,7 +0,0 @@
-namespace NuiN.ScriptableHarmony.Internal.Helpers
-{
-    internal enum Access { Getter, Setter }
-    internal enum SOType { Variable, ListVariable, DictionaryVariable, RuntimeSet, RuntimeSingle }
-    internal enum ResetOn { ExitPlayMode, SceneLoad }
-}
-
